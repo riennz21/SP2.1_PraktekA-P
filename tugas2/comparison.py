feb9d8db65a16b4af23b82c6cd4ad69b6b91f733
riennz21 <arienkiy@gmail.com>
2025-03-17 17:13:25 +0700

    Add files via upload

---
+++ tugas2/comparison.py
@@ -0,0 +1,23 @@
+#oprator >
+print('apakah benar 5>3 ?' ,5>3)
+print('apakah benar 5>5 ?' ,5>5)
+
+#oprator <
+print('apakah benar 5<3 ?' ,5<3)
+print('apakah benar 5<5 ?' ,5<5)
+
+#oprator >=
+print('apakah benar 5>=3 ?' ,5>=3)
+print('apakah benar 5>=5 ?' ,5>=5)
+
+#oprator <=
+print('apakah benar 5<=3 ?' ,5<=3)
+print('apakah benar 5<=5 ?' ,5<=5)
+
+#oprator ==
+print('apakah benar 5==3 ?' ,5==3)
+print('apakah benar 5==5 ?' ,5==5)
+
+#oprator !=
+print('apakah benar 5!=3 ?' ,5!=3)
+print('apakah benar 5!=5 ?' ,5!=5)
