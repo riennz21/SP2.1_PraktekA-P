0c1bd0edfd1d3b15cfc19757175b7e3760181712
riennz21 <arienkiy@gmail.com>
2025-03-17 17:06:04 +0700

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,39 @@
+#buat 2 variabel
+a = 5
+b = 3
+
+#penjumlahana(+)
+hasil = a + b 
+print('hasil penjumlahan :',hasil)
+
+#pengurangan(-)
+hasil = a - b
+print('hasil pengurangan :',hasil)
+
+#perkalian(*)
+hasil = a * b 
+print('hasil perkalian : ',hasil)
+
+#pangkat (**)
+hasil = a ** b
+print('hasil pangkat :',hasil)
+
+#pembagian(/)
+hasil = a / b 
+print('hasil pembagaian :',hasil)
+
+# pembagian kebawah  (//)
+hasil = a // b
+print('hasil pembagian kebawah:',hasil)
+
+#sisa bagi (%)
+hasil = a % b
+print('hasil sisa bagi :',hasil)
+
+#precedence
+hasil = 1 + 2 * 4 - 3
+print('hasil precendence :',hasil)
+
+#precendence 
+hasil = (1 + 2) * 4 - 3
+print('hasl predence :',hasil)
