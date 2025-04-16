586780981d52b4827cf9b525da6849bffaf23003
riennz21 <arienkiy@gmail.com>
2025-04-16 20:56:15 +0700

    Add files via upload

---
+++ pertemuan 5/whileloop_forloop.py
@@ -0,0 +1,12 @@
+#while loop
+#print('pengulangan ke : 1 ')
+#print('pengulangan ke : 2 ')
+#print('pengulangan ke : 3 ')
+
+nilaiawal = 1
+while nilaiawal <= 1000 :
+    print('pengulangan ke :',nilaiawal)
+    nilaiawal += 1
+    #nilaiawal = nilaiawal + 1
+
+    
